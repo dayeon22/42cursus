6db3dc4b0355fcc389366989f93cf7c56c8976d5
Dayeon Park <ddggs@naver.com>
2021-09-25 23:59:22 +0900

    make push

--- push_swap/main.c
+++ push_swap/main.c
@@ -1,6 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daypark <daypark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/25 19:48:46 by daypark           #+#    #+#             */
+/*   Updated: 2021/09/25 23:58:01 by daypark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
+
+void	push(t_data **data, int num)
+{
+	t_data	*lst;
+	t_data	new;
+
+	lst = *data;
+	if (lst == NULL)
+	{
+		new.value = num;
+		new.pre = NULL;
+		new.next = NULL;
+		*data = &new;
+		return ;
+	}
+	lst = *data;
+	lst->pre = &new;
+	new.value = num;
+	new.pre = NULL;
+	new.next = lst;
+}
+
+void	print_values(t_data **data)
+{
+	t_data	*temp;
+
+	temp = *data;
+	printf("%d", temp->value);
+//	while (temp != NULL)
+//	{
+//		printf("%d -> ", temp->value);
+//		temp = temp->next;
+//	}
+}
 
 int	main()
 {
-	ft_putendl_fd("push_swap first", 1);
+	t_data *a;
+
+	a = NULL;
+	push(&a, 1);
+	push(&a, 2);
+	push(&a, 3);
+	push(&a, 4);
+	push(&a, 5);
+	print_values(&a);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -1,6 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daypark <daypark@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/25 19:48:55 by daypark           #+#    #+#             */
+/*   Updated: 2021/09/25 23:19:48 by daypark          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
 #include "./libft/libft.h"
+#include <stdio.h> //지우기
+
+typedef struct	s_data
+{
+	int				value;
+	struct s_data	*pre;
+	struct s_data	*next;
+}				t_data;
 
 #endif
