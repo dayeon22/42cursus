a3bef39fc155a39ad63f3cfdb8d65f6389cb8d09
Dayeon Park <ddggs@naver.com>
2021-09-27 11:35:03 +0900

    push and pop

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,55 +6,77 @@
 /*   By: daypark <daypark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 19:48:46 by daypark           #+#    #+#             */
-/*   Updated: 2021/09/25 23:58:01 by daypark          ###   ########.fr       */
+/*   Updated: 2021/09/27 11:34:36 by daypark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push(t_data **data, int num)
+void	push(t_stack *stack, int num)
 {
-	t_data	*lst;
-	t_data	new;
+	t_node *node;
 
-	lst = *data;
-	if (lst == NULL)
+	node = (t_node *)malloc(sizeof(t_node));
+	node->value = num;
+	if (stack->size == 0)
 	{
-		new.value = num;
-		new.pre = NULL;
-		new.next = NULL;
-		*data = &new;
-		return ;
+		node->pre = NULL;
+		node->next = NULL;
+		stack->top = node;
+		stack->bottom = node;
 	}
-	lst = *data;
-	lst->pre = &new;
-	new.value = num;
-	new.pre = NULL;
-	new.next = lst;
+	else
+	{
+		node->pre = stack->bottom;
+		node->next = NULL;
+		stack->bottom->next = node;
+		stack->bottom = node;
+	}
+	stack->size++;
 }
 
-void	print_values(t_data **data)
+int	pop(t_stack *stack)
 {
-	t_data	*temp;
+	int		ret;
+	t_node	*temp;
 
-	temp = *data;
-	printf("%d", temp->value);
-//	while (temp != NULL)
-//	{
-//		printf("%d -> ", temp->value);
-//		temp = temp->next;
-//	}
+	ret = stack->top->value;
+	temp = stack->top;
+	stack->top = stack->top->next;
+	free(temp);
+	return (ret);
+}
+
+void	print_stack(t_stack *stack)
+{
+	t_node	*temp;
+
+	temp = stack->top;
+	while (temp != NULL)
+	{
+		printf("%d -> ", temp->value);
+		temp = temp->next;
+	}
+}
+
+void	init_stack(t_stack *stack)
+{
+	stack->size = 0;
+	stack->top = NULL;
+	stack->bottom = NULL;
 }
 
 int	main()
 {
-	t_data *a;
+	t_stack a;
 
-	a = NULL;
+	init_stack(&a);
 	push(&a, 1);
 	push(&a, 2);
 	push(&a, 3);
 	push(&a, 4);
 	push(&a, 5);
-	print_values(&a);
+	printf("pop : %d\n", pop(&a));
+	printf("pop : %d\n", pop(&a));
+	print_stack(&a);
 }
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: daypark <daypark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/25 19:48:55 by daypark           #+#    #+#             */
-/*   Updated: 2021/09/25 23:19:48 by daypark          ###   ########.fr       */
+/*   Updated: 2021/09/26 18:10:45 by daypark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@
 #include "./libft/libft.h"
 #include <stdio.h> //지우기
 
-typedef struct	s_data
+typedef struct	s_node
 {
 	int				value;
-	struct s_data	*pre;
-	struct s_data	*next;
-}				t_data;
+	struct s_node	*pre;
+	struct s_node	*next;
+}				t_node;
+
+typedef struct	s_stack
+{
+	int		size;
+	t_node	*top;
+	t_node	*bottom;
+}				t_stack;
 
 #endif
