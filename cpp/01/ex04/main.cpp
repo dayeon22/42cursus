81ac055a1b4089bff924dc66b9a635399cd9b26c
Dayeon Park <ddggs@naver.com>
2022-02-16 11:50:10 +0900

    module01 ex04

--- cpp/01/ex04/main.cpp
+++ cpp/01/ex04/main.cpp
@@ -6,46 +6,49 @@
 /*   By: daypark <daypark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 18:17:39 by daypark           #+#    #+#             */
-/*   Updated: 2022/02/16 11:17:41 by daypark          ###   ########.fr       */
+/*   Updated: 2022/02/16 11:47:35 by daypark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 
+int printErr(std::stirng errMsg){
+	std::cerr << errMsg << std::endl;
+	return 0;
+}
+
 int main(int argc, char *argv[]){
 	if (argc != 4){
-		std::cerr << "usage: ./sed file_name str_to_be_replaced str_to_replace" << std::endl;
-		return 0;
+		return (printErr("usage: ./sed file_name str_to_be_replaced str_to_replace"));
 	}
 	std::ifstream fin(argv[1]);
 	if (fin.fail()){
-		std::cerr << "failed to read file" << std::endl;
-		return 0;
+		return (printErr("failed to read file"));
 	}
 	std::string replaceFile = argv[1];
 	replaceFile += ".replace";
 	std::ofstream fout(replaceFile);
 	if (fout.fail()){
-		std::cerr << "failed to make .replace file" << std::endl;
 		fin.close();
-		return 0;
+		return (printErr("failed to make .replace file"));
 	}
 
 	std::string line;
-	size_t index;
 	while (true){
 		std::getline(fin, line);
 
+		size_t start = 0;
+		size_t index = 0;
 		while (true){
-			index = line.find(argv[2]);
+			index = line.find(argv[2], start);
 			if (index == std::string::npos){
-				fout << line.substr(0, std::string::npos);
+				fout << line.substr(start);
 				break;
 			}
-			fout << line.substr(0, index);
+			fout << line.substr(start, index - start);
 			fout << argv[3];
-			line = line.substr(index + std::strlen(argv[2]));
+			start = index + std::strlen(argv[2]);
 		}
 		if (fin.eof())
 			break;
