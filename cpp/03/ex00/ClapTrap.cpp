e240d5fc73ebca85b10076df02bcdb8b9af95734
git43210 <ddggs@naver.com>
2022-03-28 10:03:57 +0900

    ex00

--- cpp/03/ex00/ClapTrap.cpp
+++ cpp/03/ex00/ClapTrap.cpp
@@ -6,28 +6,29 @@
 /*   By: daypark <daypark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 11:02:19 by daypark           #+#    #+#             */
-/*   Updated: 2022/03/24 13:17:13 by daypark          ###   ########.fr       */
+/*   Updated: 2022/03/28 09:50:20 by daypark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : name_("claptrap"), hitPoints_(10), energePoints_(10), attackDamage_(0) {
-	std::cout << "[" << name_ << "] " << "is created by default constructor." << std::endl;
+ClapTrap::ClapTrap() : hitPoints_(10), energePoints_(10), attackDamage_(0) {
+	name_ = "claptrap";
+	std::cout << "[" << name_ << "]" << "is created by default constructor." << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name) : hitPoints_(10), energePoints_(10), attackDamage_(0) {
 	name_ = name;
-	std::cout << "[" << name_ << "] " << "is created by constructor." << std::endl;
+	std::cout << "[" << name_ << "]" << "is created by constructor." << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &clapTrap) {
 	*this = clapTrap;
-	std::cout << "[" << name_ << "] " << "is copied." << std::endl;
+	std::cout << "[" << name_ << "]" << "is copied." << std::endl;
 }
 
 ClapTrap::~ClapTrap() {
-	std::cout << "[" << name_ << "] " << "is deleted." << std::endl;
+	std::cout << "[" << name_ << "]" << "is deleted." << std::endl;
 }
 
 ClapTrap &ClapTrap::operator=(const ClapTrap &clapTrap) {
@@ -40,36 +41,33 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &clapTrap) {
 }
 
 void ClapTrap::attack(const std::string &target) {
-	// 타겟이 데미지를 입어야함 attakDamage만큼 데미지를 입어야 함
-	//target
-	//takeDamage(attackDamage_);
 	if (hitPoints_ <= 0 || energePoints_ <= 0) {
-		std::cout << "[" << name_ << "] " << "cannot do anything." << std::endl;
+		std::cout << "[" << name_ << "]" << "cannot do anything." << std::endl;
 		return ;
 	}
 	energePoints_--;
-	std::cout << "[" << name_ << "] " << "attacks " << target << "causing " << attackDamage_ << " points of damage." << std::endl;
+	std::cout << "[" << name_ << "]" << "attacks " << target << " causing " << attackDamage_ << " points of damage." << std::endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount) {
 	if (hitPoints_ <= 0) {
-		std::cout << "[" << name_ << "] " << "is already dead." << std::endl;
+		std::cout << "[" << name_ << "]" << "is already dead." << std::endl;
 	}
 	if (hitPoints_ <= amount) {
 		hitPoints_ = 0;
-		std::cout << "[" << name_ << "] " << "dies." << std::endl;
+		std::cout << "[" << name_ << "]" << "dies." << std::endl;
 		return ;
 	}
 	hitPoints_ -= amount;
-	std::cout << "[" << name_ << "] " << "is damaged " << amount << " hit point." << std::endl;
+	std::cout << "[" << name_ << "]" << "is damaged " << amount << " hit point." << std::endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount) {
 	if (hitPoints_ <= 0 || energePoints_ <= 0) {
-		std::cout << "[" << name_ << "] " << "cannot do anything." << std::endl;
+		std::cout << "[" << name_ << "]" << "cannot do anything." << std::endl;
 		return ;
 	}
 	energePoints_--;
 	hitPoints_ += amount;
-	std::cout << "[" << name_ << "] " << "repairs " << amount << " hit point." << std::endl;
+	std::cout << "[" << name_ << "]" << "repairs " << amount << " hit point." << std::endl;
 }
--- cpp/03/ex00/main.cpp
+++ cpp/03/ex00/main.cpp
@@ -5,8 +5,32 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: daypark <daypark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/24 11:02:22 by daypark           #+#    #+#             */
-/*   Updated: 2022/03/24 11:02:23 by daypark          ###   ########.fr       */
+/*   Created: 2022/03/28 09:29:43 by daypark           #+#    #+#             */
+/*   Updated: 2022/03/28 10:03:21 by daypark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
+
+int main() {
+	ClapTrap ct1("ct1");
+	ClapTrap ct2(ct1);
+	ClapTrap ct3;
+
+	ct1.attack("ct0");
+	ct1.takeDamage(3);
+	ct1.beRepaired(5);
+	ct1.takeDamage(20);
+	ct1.beRepaired(10);
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+	ct3.attack("ct0");
+}
