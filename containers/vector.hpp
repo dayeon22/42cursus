82a254b1c83b30dbc0fd98c865e317fc8fd802d7
Dayeon Park <ddggs@naver.com>
2022-09-18 16:14:07 +0900

    42 final commit

--- containers/vector.hpp
+++ containers/vector.hpp
@@ -7,18 +7,18 @@ namespace ft{
 	
 	template < class T, class Alloc = std::allocator<T> > class vector{
 		public:
-			typedef T								value_type;
-			typedef Alloc							allocator_type;
-			typedef value_type&						reference;
-			typedef const value_type&				const_reference;
-			typedef allocator_type::pointer			pointer;
-			typedef allocator_type::const_pointer	const_pointer;
+			typedef T					value_type;
+			typedef Alloc				allocator_type;
+			typedef value_type&			reference;
+			typedef const value_type&	const_reference;
+			typedef value_type*			pointer;
+			typedef const value_type*	const_pointer;
 			// iterator;
 			// const_iterator;
 			// reverse_iterator;
 			// const_reverse_iterator;
-			typedef std::ptrdiff_t					difference_type;
-			typedef std::size_t						size_type;
+			typedef std::ptrdiff_t		difference_type;
+			typedef std::size_t			size_type;
 
 		private:
 			pointer			_start;
